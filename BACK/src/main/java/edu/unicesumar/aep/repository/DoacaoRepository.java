9ad971158aa25564ee0cfcab94add0ad1651c0f7
Hugo <164659094+isu-Hugo@users.noreply.github.com>
2026-09-07 17:04:09 -0300

    Revert "Chore/kan 6 implementacao service"

--- BACK/src/main/java/edu/unicesumar/aep/repository/DoacaoRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.unicesumar.aep.repository;
-
-import edu.unicesumar.aep.model.DoacaoModel;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DoacaoRepository extends MongoRepository<DoacaoModel, String> {
-}
